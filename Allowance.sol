b55a0a2f8996cb11f7444a097a57b68fce5bc3e1
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2021-12-07 12:14:49 -0800

    added who can withdraw and how much

--- Allowance.sol
+++ Allowance.sol
@@ -4,6 +4,16 @@ pragma solidity ^0.8.10;
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract AllowanceWallet is Ownable {
+    //we need some sort of data struction to hold an unsigned integer to an address
+    mapping(address => uint256) public allowance;
+
+    // who is allowed to withdraw and how much are they allowed to withdraw
+    function addAllowance(address _who, uint256 _amount) public onlyOwner {
+        //set the allowance for the person to the amount
+        allowance[_who] = _amount;
+    }
+
+    //this code below was what we wrote before using the openzeppelin library for the Ownable contract
     // //ensure only the owner can send money
     // address public owner;
 
