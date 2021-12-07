0f538b6a92864a4a160179a4c4a87012578be732
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2021-12-06 17:03:56 -0800

    refactored with modifier

--- Modifier.sol
+++ Modifier.sol
@@ -1,26 +1,42 @@
 pragma solidity ^0.8.10;
 
-contract InheritanceModifierExample {
-    mapping(address => uint256) public tokenBalance;
-
+contract Owned {
     address owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "you are not allowed");
+        _;
+    }
+}
+
+//'is Owned' inherits from the contract above
+contract InheritanceModifierExample is Owned {
+    mapping(address => uint256) public tokenBalance;
 
     uint256 tokenPrice = 1 ether;
 
     //we are creating a token and we give the owner of the contract 100 tokens to begin with
     constructor() public {
-        owner = msg.sender;
         tokenBalance[owner] = 100;
     }
 
+    //this is used to create code once and use multiple times elsewhere
+    //'don't repeat yourself'
+    // modifier onlyOwner() {
+    //     require(msg.sender == owner, "you are not allowed");
+    //     _;
+    // }
+
     //if you are not the owner you can buy tokens and each token costs 1 ether
-    function createNewToken() public {
-        require(msg.sender == owner, "you are not allowed");
+    function createNewToken() public onlyOwner {
         tokenBalance[owner]++;
     }
 
-    function burnToken() public {
-        require(msg.sender == owner, "you are not allowed");
+    function burnToken() public onlyOwner {
         tokenBalance[owner]--;
     }
 
